65d6b889367a6de7f9eb1bf71588ce3bab745954
Jonathan Dorety <kyullog@gmail.com>
2019-02-07 10:56:12 -0500

    Basic store setup

---
+++ friends-redux/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  test: "this is a test"
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
