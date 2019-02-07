884bae37c45649139defdcf30f8df46c95a3d741
Jonathan Dorety <kyullog@gmail.com>
2019-02-07 16:27:11 -0500

    Display friend information correctly to screen

--- friends-redux/src/components/Friends.js
+++ friends-redux/src/components/Friends.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Friends = () => {
-  return <h2>This is a test</h2>;
+const Friends = props => {
+  return props.friends.map(friend => {
+    return (
+      <div className="friend-card" key={friend.id}>
+        <h3>{friend.name}</h3>
+        <p>Age: {friend.age}</p>
+        <p>Email: {friend.email}</p>
+      </div>
+    );
+  });
 };
 
-export default Friends;
+const mapStateToProps = state => {
+  return {
+    friends: state.friends
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Friends);
