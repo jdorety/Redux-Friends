9b33b5582d92a301c609286fc449a2712127807d
Jonathan Dorety <kyullog@gmail.com>
2019-02-07 22:01:54 -0500

    Give visual feedback about loading state

--- friends-redux/src/components/Friends.js
+++ friends-redux/src/components/Friends.js
@@ -2,20 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 
 const Friends = props => {
-  return props.friends.map(friend => {
-    return (
-      <div className="friend-card" key={friend.id}>
-        <h3>{friend.name}</h3>
-        <p>Age: {friend.age}</p>
-        <p>Email: {friend.email}</p>
-      </div>
-    );
-  });
+  return (
+    <div className="friends-list">
+      {props.loading && <h2>Loading</h2>}
+
+      {props.friends.map(friend => {
+        return (
+          <div className="friend-card" key={friend.id}>
+            <h3>{friend.name}</h3>
+            <p>Age: {friend.age}</p>
+            <p>Email: {friend.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
   return {
-    friends: state.friends
+    friends: state.friends,
+    loading: state.loading
   };
 };
 
